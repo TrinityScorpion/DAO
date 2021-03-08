11cd3c9ff476315aa9ddd23b0b1c205d25940f1f
Mariusz Kępa <TrinityScorpion>
2021-03-08 21:40:36 +0100

    adding database

---
+++ database.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE dao_db
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+use dao_db;
+
+CREATE TABLE users(
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email varchar(255) NOT NULL UNIQUE,
+    username varchar(255) NOT NULL,
+    password varchar(60) NOT NULL
+);
+
+
+
+
+
